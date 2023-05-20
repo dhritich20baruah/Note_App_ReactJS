a356a0e955165a83896d11ae3b26d64f074c0422
Dhriti <dhriti.baruah20@gmail.com>
2023-05-20 21:34:41 +0530

    restored

--- reactnote/src/components/Home.js
+++ reactnote/src/components/Home.js
@@ -1,25 +1,11 @@
-import React, {useState} from 'react'
-import SignIn from './SignIn'
-import SignUp from './SignUp'
+import React from 'react'
+
 
 const Home = () => {
-    const [visibility, setVisibility] = useState(false)
-    const handleVisible = () => {
-        setVisibility(visibility => !visibility)
-    }
+  
   return (
-    <div className='container'>
-        {visibility ? 
-        <div>
-        <SignUp/>
-        <p>Already registered? <span onClick={handleVisible}>SignIn Here</span></p>
-        </div>
-        :
-        <div>
-        <SignIn/>
-        <p>Not registered? <span onClick={handleVisible}>SignUp Here</span></p>
-        </div>       
-        }
+    <div>
+      Home
     </div>
   )
 }
